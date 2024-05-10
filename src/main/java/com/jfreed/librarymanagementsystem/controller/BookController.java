04d08cc07da3244f6c3c84eb8c9b894f7a6ae9b1
james.freed <jamesfreed1994@hotmail.com>
2024-05-10 09:07:52 +0100

    Add new book form

--- src/main/java/com/jfreed/librarymanagementsystem/controller/BookController.java
+++ src/main/java/com/jfreed/librarymanagementsystem/controller/BookController.java
@@ -9,6 +9,9 @@ import org.springframework.ui.Model;
 import com.jfreed.librarymanagementsystem.entity.Book;
 import com.jfreed.librarymanagementsystem.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @Controller
@@ -18,10 +21,24 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/books")
-    public String allBooks(Model model) {
+    public String index(Model model) {
         List<Book> books = bookService.findAll();
         model.addAttribute("books", books);
         return "books/index";
     }
+
+    @GetMapping("/books/add")
+    public String create(Model model) {
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "books/create";
+    }
+
+    @PostMapping("/books/add")
+    public String save(@ModelAttribute("book") Book book, Model model) {
+        bookService.save(book);
+        return "redirect:/books";
+    }
+    
     
 }
--- src/main/java/com/jfreed/librarymanagementsystem/service/BookService.java
+++ src/main/java/com/jfreed/librarymanagementsystem/service/BookService.java
@@ -17,4 +17,8 @@ public class BookService {
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
+
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
 }
